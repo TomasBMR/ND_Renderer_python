55bd09057ac228974ccd3cdab4bdb40f0d866051
TomasBMR <tomasbmribeiro@gmail.com>
2024-07-25 19:18:07 -0300

    Revert "Projeções organizadas, cores e importadores"
    This reverts commit 6b808e99a022808356cb1fef818bfb6e897a2679.

--- Main.py
+++
@@ -1,6 +0,0 @@
-from pyEngine.Gerenciador import Gerenciador
-from MainCena import MainCena
-
-largura, altura = 600, 450
-
-Gerenciador(largura, altura, MainCena).Run()
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    print("[", end="")
-    for face in facesDims:
-        print("[", end="")
-        for i in face:
-            print(i, ",")
-        print("],")
-    print("]")
